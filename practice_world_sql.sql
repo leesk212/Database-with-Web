0c38eb9831668de18cc716fa68c67f548421966c
leesk212@gmail.com <leesk212@gmail.com>
2020-08-30 19:09:39 +0900

    20/08/30

--- practice_world_sql.sql
+++ practice_world_sql.sql
@@ -1,4 +1,3 @@
-DESCRIBE city;
 DESC country;		  
 DESC countrylanguage; #Field,Type,NULL,Key등을 알 수 있다.
 SELECT * FROM city; #city에 대한 전체 열들의 값들을 볼 수 있다. 
@@ -10,4 +9,6 @@ WHERE Population < 8000000 #8000000만 이상의 사람들만 보고 싶을 때 
 AND Population > 7000000;	#관계연산자: NOT,AND,OR등을 써서
 							#700000만 이상인데 80000000만 이하인 사람들을 찾아 낼 수 있다.
 							#조건과 관계연산자를 이용해 내가 원하는 Data들을 뽑아낼 수 있다
-                            
+
+
+DESC 
